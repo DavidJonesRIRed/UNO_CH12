7b5199f70d0fea2f25bed4dfad705a26e9675153
David Jones <coreldavid@hotmail.com>
2021-10-19 17:45:34 -0400

    Added Main

---
+++ src/com/DavidJones/Main.java
@@ -0,0 +1,18 @@
+package com.DavidJones;
+
+import com.DavidJones.deck.Card;
+import com.DavidJones.deck.Deck;
+import com.DavidJones.deck.StandardUnoDeck;
+import com.DavidJones.uno.Hand;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Deck deck = new StandardUnoDeck();
+        deck.shuffle();
+        Hand myHand = new Hand();
+        myHand.addCard(deck.draw());
+        myHand.displayHand();
+    }
+}
